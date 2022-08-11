ee6f21a037df67f4616967874423c83071fe242c
Ignacio <ignaciojsoler@gmail.com>
2022-08-11 10:26:03 -0300

    ejercicio 4 python

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+# Ejercicio 4)
+
+# Escribir el código en Python que permita realizar lo siguiente:
+
+# Crear un lista en Python denominado “Perro2” que contenga los siguientes valores:
+
+#   14,  Fido,  12/12/2012 , Macho, 23546987
+
+perro2 = [14, 'Fido', '12/12/2012', 'Macho', 23546987]
+
+for i in perro2:
+    print(i)
