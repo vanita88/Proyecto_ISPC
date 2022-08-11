8d552d0ad117256937e9542fb5f76119c05ce5c4
Morgwens <95719903+Morgwens@users.noreply.github.com>
2022-08-11 11:43:34 -0300

    Update and rename ejercicio1.py to Carrazan-ejercicio1.py

--- Carrazan-ejercicio1.py
+++ Carrazan-ejercicio1.py
@@ -7,7 +7,7 @@
   #  28957346,  Juan,  Perez, 4789689,  Belgrano 101
 
  # Dichos datos se corresponden  con los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección). Muestre en pantalla el teléfono del dueño si el DNI es mayor a 26000000.
- 
+ #María José Carrazán
 dueno = ["Juan", "Perez",28957346, 4789689, "Belgrano 101"]
 
 for i in dueno:
