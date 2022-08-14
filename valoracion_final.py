4d8ee50780ab048bb367437ba2bc4d766697f23a
Gisella Ros <89737662+GiseRos@users.noreply.github.com>
2022-08-14 00:58:39 -0300

    Add files via upload

---
+++ valoracion_final.py
@@ -0,0 +1,12 @@
+historial_puppy = ('2350', '5960', '23000', '1000', '8900')
+
+historial_puppy_total = (2350 + 5960 + 23000 + 1000 + 8900)
+
+print(historial_puppy)
+
+if historial_puppy_total <= 30000:
+    print (historial_puppy_total)
+else:
+    print ('Gastos por encima de lo presupuestado')
+
+    
